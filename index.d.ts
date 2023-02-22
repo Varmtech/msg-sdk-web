9fd5545f4465dcbdfb6675819c03fd1eff2eb49e
armensct <armen@sceyt.com>
2023-02-22 11:05:13 +0400

    Implement forward message

--- index.d.ts
+++ index.d.ts
@@ -420,6 +420,7 @@ declare class MessageBuilder {
   setMentionUserIds: (userIds: string[]) => this;
   setParentMessageId: (messageId: string) => this;
   setReplyInThread: () => this;
+  setForwardingMessageId: (messageId: string) => this;
   create: () => Message;
 }
 
@@ -575,6 +576,12 @@ interface Message {
   replyCount?: number;
   transient: boolean;
   silent: boolean;
+  forwardingDetails?: {
+    channelId: string
+    hops: number
+    messageId: string
+    user: User
+  }
 
   mentionUserIds: () => string[];
 }
